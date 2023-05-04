b4f9f1a47effc6104327b3de51d94cef2d4385b8
Ali Mammadov <98839702+AliMammadovv@users.noreply.github.com>
2023-05-04 22:00:47 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from find_max_function import find_max
+numbers = [10, 3, 6, 2, 20] # creating a random list in order to test our code
+# maximum = find_max(numbers)
+print(find_max(numbers))
+
+
+
+print()
+
